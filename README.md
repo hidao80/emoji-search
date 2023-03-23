fb00fd3f60d8adf6f6444754ee358d05c8902e96
hidao80 <8155294+hidao80@users.noreply.github.com>
2023-03-23 10:07:07 +0900

    first commit.🎉

---
+++ README.md
@@ -0,0 +1,21 @@
+[![AGPL-3.0 license](https://img.shields.io/badge/license-AGPL--3.0-blue.svg?style=flat)](https://opensource.org/licenses/AGPL-3.0)
+
+# Emoji search
+
+You can search for pictograms on the web page.
+
+## LICENSE
+
+AGPL-3.0
+
+## Codes used
+
+### emojilist.json
+
+copyright Copyright Mathias Bynens <https://mathiasbynens.be/>
+license the MIT AGPL-3.0
+
+### Emoji regular expression
+
+copyright Copyright Mathias Bynens <https://mathiasbynens.be/>
+license the MIT license
---
+++ js/main.js
@@ -0,0 +1,75 @@
+import emoji2HTMLEntity from './emoji2htmlCode.js';
+
+window.onload = e => {
+    const jsonUrl = "../assets/emojilist.json";
+
+    // loading emojis
+    fetch(jsonUrl)
+    .then( response => response.json())
+    .then(emojis => {
+        // Set emoji categories as search candidates.
+        const items = emojis.filter(
+            (element, index, self) => self.findIndex((e) => e.category === element.category) === index
+        );
+        const datalist = document.querySelector("datalist");
+        for (const item of items) {
+            const categoryOption = document.createElement("Option");
+            categoryOption.value = item.category;
+
+            datalist.appendChild(categoryOption);
+        }
+
+        // Display of pictogram list
+        const container = document.querySelector('container');
+        for (const emoji of emojis) {
+            const containerDiv = document.createElement("div");
+            containerDiv.classList.add("item");
+            containerDiv.style = "margin:5px;width:calc(12.5% - 6px)";
+            containerDiv.dataset.keywords = emoji.keywords.join(",");
+
+            const emojiDiv = document.createElement("div");
+            emojiDiv.textContent = emoji.char;
+
+            const nameDiv = document.createElement("div");
+            nameDiv.textContent = emoji.name;
+
+            const htmlCodeDiv = document.createElement("div");
+            htmlCodeDiv.textContent = emoji2HTMLEntity(emoji.char);
+
+            const hexDiv = document.createElement("div");
+            hexDiv.textContent = emoji2HTMLEntity(emoji.char).replace(/(&#x|;)/ig, "");
+
+            containerDiv.appendChild(emojiDiv);
+            containerDiv.appendChild(nameDiv);
+            containerDiv.appendChild(htmlCodeDiv);
+            containerDiv.appendChild(hexDiv);
+
+            container.appendChild(containerDiv);
+        }
+    });
+
+    // Hide items that are not included in the search
+    const searchButton = document.querySelector('button');
+
+    // Search button action
+    searchButton.addEventListener('click', e => {
+        const query = document.querySelector('#emojiSearch').value;
+
+        const items = document.querySelectorAll(".item");
+        for (const item of items) {
+            if (item.dataset.keywords.indexOf(query) == -1) {
+                item.classList.add("d-none");
+            } else {
+                item.classList.remove("d-none");
+            }
+        }
+    });
+
+    // Search textbox action
+    document.querySelector('#emojiSearch').addEventListener('keypress', e => {
+        if (e.key === 'Enter') {
+            searchButton.click();
+        }
+        return false;
+    });
+}
